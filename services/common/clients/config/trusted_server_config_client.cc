bfc96ba0ba856fbd3158c7881b7e7cbaacdcb970
Ken Gordon <ken.gordon@microsoft.com>
2024-01-10 13:07:57 +0000

    Only populate secondary endpoints if the config is set
    Co-Authored-By: Dominic Ayre <dominicayre@microsoft.com>

--- services/common/clients/config/trusted_server_config_client.cc
+++ services/common/clients/config/trusted_server_config_client.cc
@@ -109,7 +109,7 @@ absl::Status TrustedServersConfigClient::Init(
 
 bool TrustedServersConfigClient::HasParameter(
     absl::string_view name) const noexcept {
-  return config_entries_map_.contains(name);
+  return config_entries_map_.contains(name) && config_entries_map_.at(name) != kEmptyValue;
 }
 
 absl::string_view TrustedServersConfigClient::GetStringParameter(
